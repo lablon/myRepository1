17682639f82f4e3157388397246a050d7b071040
Labinko_O <Labinko_O@Labinko_O.iba>
2017-12-08 15:02:37 +0300

    Commit new branch
    First branch commit and push

--- src/myProject1/myClass1.java
+++ src/myProject1/myClass1.java
@@ -4,6 +4,7 @@ public class myClass1 {
     public static void main(String[] args) {
         // Display "Hello World!"
     	System.out.println("Hello World! How are you?");
+    	System.out.println("New branch...");
     }
 
 }
