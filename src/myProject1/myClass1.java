b98ed28baf2c72a0492583182be51fecbab1d12c
Labinko_O <Labinko_O@Labinko_O.iba>
2017-12-08 14:53:03 +0300

    to myRepository1 first commit
    myClass1 first commit

---
+++ src/myProject1/myClass1.java
@@ -0,0 +1,9 @@
+package myProject1;
+
+public class myClass1 {
+    public static void main(String[] args) {
+        // Display "Hello World!"
+    	System.out.println("Hello World! How are you?");
+    }
+
+}
