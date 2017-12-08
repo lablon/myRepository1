f58368f461e040370ef5325750f6c65a7540a0e8
Labinko_O <Labinko_O@Labinko_O.iba>
2017-12-08 15:22:41 +0300

    build-file
    build-file commit and push

--- src/myProject1/myClass1.java
+++ src/myProject1/myClass1.java
@@ -5,6 +5,7 @@ public class myClass1 {
         // Display "Hello World!"
     	System.out.println("Hello World! How are you?");
     	System.out.println("New branch...");
+    	System.out.println("O! new build file!");
     }
 
 }
